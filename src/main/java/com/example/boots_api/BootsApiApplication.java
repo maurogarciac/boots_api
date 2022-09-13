d2e3ab21972e898c21881bbb2a81157c7193eb13
maurogarciac <maurogarciacoto@gmail.com>
2022-09-12 21:25:11 -0300

    everything runs now

---
+++ src/main/java/com/example/boots_api/BootsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.boots_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boots_api/BootsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boots_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
